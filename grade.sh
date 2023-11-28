9e02d14672a0cace88c1578ed04fc4ce29848b0e
MinQuach <m2quach@ucsd.edu>
2023-11-28 11:16:30 -0800

    Update grade script

--- grade.sh
+++ grade.sh
@@ -11,6 +11,27 @@ echo 'Finished cloning'
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
+file='student-submission/ListExamples.java'
+if [[ -e $file ]] && [[ -f $file ]]
+then 
+    echo "Found ListExamples.java file"
+else 
+    echo "Can't find ListExamples.java file"
+    exit 1
+fi
+
+cp -r lib $file TestListExamples.java grading-area
+cd grading-area
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+javac -cp $CPATH *.java TestListExamples.java
+if [[ $? -ne 0 ]]
+then 
+    echo "Compile Error"
+    exit 1
+fi
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > output.txt
+echo -n $(grep -oE "Failures: [0-9]+" output.txt | grep -oE "[0-9]+") 
+echo " /" $(grep -oE "Tests run: [0-9]+" output.txt | grep -oE "[0-9]+") 
+ 
